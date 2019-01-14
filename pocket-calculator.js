2de9aa43bf96c98ca1744f3494fdbda3afa47b20
Jose Paredes <jparedes0303@gmail.com>
2019-01-14 13:19:02 -0500

    Something

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,12 @@
+let a = firstValue
+let b = secondValue
+
 function display(num) {
   document.calc.display.value = document.calc.display.value + num;
+}
+function decimal() {
+  document.calc.display.value = document.calc.display. + ;
+}
+function equal() {
+
 }
